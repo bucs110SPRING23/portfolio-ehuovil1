185fd5ed83ab742329283aa1e70274708bfdab9d
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-01-30 17:16:03 -0500

    erik

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,12 +1,18 @@
 import random
 #Part A
 weeks = 16
-print(weeks, type(weeks)) #'var' is the variable name
+print(weeks, type(weeks)) 
 classes = 5
-print(classes, type(classes)) #'var' is the variable name
+print(classes, type(classes)) 
 tuition = 6000
+print(tuition, type(tuition))
 cost_per_week = ((tuition / classes) / weeks)
+print(cost_per_week, type(cost_per_week))
 print("Cost per week:", cost_per_week)
 classes_per_week = 2
+print(classes_per_week, type(classes_per_week))
 cost_per_class = cost_per_week / classes_per_week
+print(cost_per_class, type(cost_per_class))
 print("The cost per class is $" + str(cost_per_class))
+List = ("Hockey", "Soccer", "Basketball", "Tennis", "Football")
+print(random.choice(List))
