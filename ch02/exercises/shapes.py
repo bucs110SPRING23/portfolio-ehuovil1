b5f6ef6379805eb36c3a5fde08e1396ce383c976
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-02-07 15:03:36 -0500

    erik

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -1,3 +1,15 @@
 import pygame
-#pygame.Surface(3 width, 4 height)
-#pygame.draw.circle(Surface, "blue", [100, 100], 10)
+display = pygame.display.set_mode((600, 400))
+pygame.draw.circle(display, "white", [300, 100], 40)
+pygame.display.update()
+pygame.draw.circle(display, "white", [300, 180], 60)
+pygame.display.update()
+pygame.draw.circle(display, "white", [300, 290], 90)
+pygame.display.update()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
