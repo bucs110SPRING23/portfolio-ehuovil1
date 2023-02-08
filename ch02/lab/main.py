551c984f63bbf2fb86d355494c8ba153a1f42423
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-02-07 23:07:27 -0500

    erik

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,70 @@
+import pygame
+import math
+import random
+import turtle
+x = random.randrange(1,10)
+print(x)
+turtle1 = turtle.Turtle()
+turtle2 = turtle.Turtle()
+turtle1.penup()
+turtle2.penup()
+turtle1.goto((-100, 20))
+turtle2.goto((-100, -20))
+turtle1.pendown()
+turtle2.pendown()
+turtle1.forward(random.randrange(1,100))
+turtle2.forward(random.randrange(1,100))
+turtle1.penup()
+turtle2.penup()
+turtle1.goto((-100, 20))
+turtle2.goto((-100, -20))
+#race2
+turtle1.pendown()
+turtle2.pendown()
+turtle1.speed(1)
+turtle2.speed(1)
+turtle1.color("blue")
+turtle2.color("blue")
+for i in range(10):
+    
+    turtle1.forward(random.randrange(1,100))
+    turtle2.forward(random.randrange(1,100))
+turtle1.penup()
+turtle2.penup()
+turtle1.goto((-100, 20))
+turtle2.goto((-100, -20))
+turtle.exitonclick()
+#partB
+pygame.init()
+window = pygame.display.set_mode()
 
-#Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+xpos
+ypos
+def getpoints(num_sides, side_length):
+    points = []
+    for i in range(num_sides):
+        angle = 360/num_sides
+        radians = math.radians(angle * i)
+        x = xpos + side_length * math.cos(radians)
+        y = ypos + side_length * math.sin(radians)
+        points.append([x, y])
+        #pygame.display.flip()
+    return points
+triangle = getpoints(3, 20)
+pygame.draw.polygon(window, "blue", triangle)
+square = getpoints(4, 20)
 
+hexagon = getpoints(6, 20)
 
-#Part B
+icosagon = getpoints(20, 20)
+
+hectagon = getpoints(100, 20)
+
+circleish = getpoints(360, 20)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
