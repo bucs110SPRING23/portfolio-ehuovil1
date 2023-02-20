34489fc3efbc631b224a8498d89c348ea3c71485
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-02-20 13:17:32 -0500

    erik

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -21,8 +21,8 @@ turtle2.goto((-100, -20))
 #race2
 turtle1.pendown()
 turtle2.pendown()
-turtle1.speed(1)
-turtle2.speed(1)
+turtle1.speed(0)
+turtle2.speed(0)
 turtle1.color("blue")
 turtle2.color("blue")
 for i in range(10):
@@ -46,62 +46,88 @@ window = pygame.display.set_mode()
 #circle -ish (360 sides)
 
 
-points = []
-tnum_sides = 3
-tside_length = 18
-xpos = 30
-ypos = 40
-tangle = 360 / tnum_sides
-radians = math.radians(tangle * i)
-x = xpos + tside_length * math.cos(radians)
-y = ypos + tside_length * math.sin(radians)
-points.append([x,y])
-x2 = xpos + tside_length * math.cos(radians)
-y2 = ypos + tside_length * math.sin(radians)
-points.append([x,y])
-x3 = xpos + tside_length * math.cos(radians)
-y3 = ypos + tside_length * math.sin(radians)
-points.append(x,y)
-pygame.draw.polygon(points)
-pygame.display.flip()
-points.clear
-window.fill("blue")
-pygame.display.flip()
+#points = []
+#tnum_sides = 3
+#tside_length = 18
+#xpos = 30
+#ypos = 40
+#tangle = 360 / tnum_sides
+#radians = math.radians(tangle * i)
+#x = xpos + tside_length * math.cos(radians)
+#y = ypos + tside_length * math.sin(radians)
+#points.append([x,y])
+#x2 = xpos + tside_length * math.cos(radians)
+#y2 = ypos + tside_length * math.sin(radians)
+#points.append([x,y])
+#x3 = xpos + tside_length * math.cos(radians)
+#y3 = ypos + tside_length * math.sin(radians)
+#points.append(x,y)
+##pygame.draw.polygon(points)
+#p#ygame.display.flip()
+#p#oints.clear
+#window.fill("blue")
+#pygame.display.flip()
+#pygame.time.delay()
+#print(points)
+#while running:
+#    for event in pygame.event.get():
+#        if event.type == pygame.QUIT:
+#            running = False#
 
-print(points)
+#pygame.quit()
+
+
+xpos = 200
+ypos = 200
+def getpoints(num_sides, side_length):
+    points = []
+    for i in range(num_sides):
+        angle = 360/num_sides
+        radians = math.radians(angle * i)
+        x = xpos + side_length * math.cos(radians)
+        y = ypos + side_length * math.sin(radians)
+        points.append([x, y])
+        pygame.display.flip()
+    return points
+
+triangle = getpoints(3, 20)
+window.fill("white")
+pygame.draw.polygon(window, "blue", triangle)
+pygame.time.wait(2000)
+
+
+square = getpoints(4, 20)
+window.fill("blue")
+pygame.draw.polygon(window, "green", square)
+pygame.time.wait(2000)
+
+
+hexagon = getpoints(6, 20)
+window.fill("white")
+pygame.draw.polygon(window, "red", hexagon)
+pygame.time.wait(2000)
+
+
+icosagon = getpoints(20, 20)
+window.fill("yellow")
+pygame.draw.polygon(window, "purple", icosagon)
+pygame.time.wait(2000)
+
+
+hectagon = getpoints(100, 20)
+window.fill("red")
+pygame.draw.polygon(window, "blue", hectagon)
+pygame.time.wait(2000)
+
+circleish = getpoints(360, 20)
+window.fill("yellow")
+pygame.draw.polygon(window, "black", circleish)
+pygame.time.wait(2000)
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
 pygame.quit()
-#xpos = 50
-#ypos = 50
-#def getpoints(num_sides, side_length):
-#    points = []
- #   for i in range(num_sides):
-  #      angle = 360/num_sides
-   #     radians = math.radians(angle * i)
-    #    x = xpos + side_length * math.cos(radians)
-     #   y = ypos + side_length * math.sin(radians)
-      #  points.append([x, y])
-       # pygame.display.flip()
-    #return points
-#triangle = getpoints(3, 20)
-#pygame.draw.polygon(window, "blue", triangle)
-#square = getpoints(4, 20)
-
-#hexagon = getpoints(6, 20)
-
-#icosagon = getpoints(20, 20)
-
-#hectagon = getpoints(100, 20)
-
-#circleish = getpoints(360, 20)
-
-#while running:
- #   for event in pygame.event.get():
-  #      if event.type == pygame.QUIT:
-   #         running = False
-
-#pygame.quit()
