173a1e658e75b376e5bec29355be2f92caa28ed3
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-03-14 10:38:04 -0400

    erik

--- ch05/exercises/scope.py
+++ ch05/exercises/scope.py
@@ -2,12 +2,12 @@
 
 #num1 = int(input("Enter number "))
 
-def multiplication(num1):
+def multiplication(num1, num3):
     product=0
-    for i in range (num1): 
+    for i in range (num3): 
         product = product + num1 
     print(product)     
-print(multiplication(2))
+print(multiplication(2, 3))
 #print(multiplication())
 
 
@@ -15,7 +15,7 @@ print(multiplication(2))
 def exponent(factor, num2):
     product = 0
     for i in range (num2):
-        product = factor * factor
+        product = product + factor * factor
     print(product)
 print(exponent(3, 3))
 
