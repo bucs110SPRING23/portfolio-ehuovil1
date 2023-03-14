e89bb0ee42c044a52220e98f88e5420430746afb
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-03-14 10:31:35 -0400

    erik

--- ch05/exercises/scope.py
+++ ch05/exercises/scope.py
@@ -2,21 +2,26 @@
 
 #num1 = int(input("Enter number "))
 
-def multiplication():
+def multiplication(num1):
     product=0
     for i in range (num1): 
-        product = product+num1      
-    print("The product is: " + str(product))
-
+        product = product + num1 
+    print(product)     
+print(multiplication(2))
 #print(multiplication())
 
 
-def exponent(factor, exponent):
-    factor = input("Factor: ")
-    exponent = input("Exponent: ")
-    product = 0
-    for i in range (exponent):
-        product = factor * factor
-    print("The result is: ")
 
-print(exponent())
+def exponent(factor, num2):
+    product = 0
+    for i in range (num2):
+        product = factor * factor
+    print(product)
+print(exponent(3, 3))
+
+def square(a):
+    squared = a * a
+    print(squared)
+print(square(5))
+
+
---
+++ ch05/lab/main.py
@@ -0,0 +1,45 @@
+import pygame
+
+
+def threenp1(n):
+    count = 0
+    while n > 1.0:
+       count += 1
+       if n % 2 == 0:
+        n = int(n / 2)
+       else:
+        n = int(3 * n + 1)
+    return count
+
+def threenp1range(upperlimit):
+    threenplus1_iters_dict = {}
+
+    for n in range(2,upperlimit + 1):
+        threenplus1_iters_dict[n] = threenp1(n) 
+    return threenplus1_iters_dict
+
+threenp1(100)
+print(threenp1range(100).items())
+print(threenp1(100))
+
+screen = pygame.display.set_mode((500, 500))
+pygame.display.update()
+pygame.draw.lines(screen, "white", False, list(threenp1range(100).items()))
+pygame.display.update()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
+
+
+
+
+
+
+
+
+
