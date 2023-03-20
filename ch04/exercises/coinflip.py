b7515574f4ae7cf649a0bb76589f84f4893d5321
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-03-20 17:38:14 -0400

    erik

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -9,14 +9,16 @@ tails = 1
 bob.penup()
 bob.goto(0, 0)
 #
-for i in range (0, 10):
-    flip = random.randrange(0, 1)
+for i in range (10):
+    flip = random.randint(0,1)
+    print(flip)
     if flip == 0:
         bob.left(90)
         bob.forward(50)
-        turtle.exitonclick()
-    if flip == 1:
+    elif flip == 1:
         bob.right(90)
         bob.forward(50)
-        turtle.exitonclick()
+
+
+turtle.exitonclick()
 
