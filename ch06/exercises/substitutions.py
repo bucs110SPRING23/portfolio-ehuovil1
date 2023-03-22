765fb5849f1f45e49037dfedbcde0e9bd6bc637f
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-03-22 16:55:39 -0400

    erik

--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,10 +1,12 @@
-{
-    "white house":"Pit of Despair",
-    "allegedly":"totally",
-    "bill":"tiktok I forgot to save",
-    "official":"puppy",
-    "congressional":"spaaaaace",
-    "republican":"piano accordionist",
-    "democrat":"chromatic button accordionist",
-   
-}
+import json
+
+def main():
+    text_fptr = open('news.txt', 'r').read()
+    sub_fptr = open('subs.json', 'r')
+    subs = json.load(sub_fptr)
+    print(subs, type(subs))
+
+
+    for i in text:
+        text_fptr+text_fptr.replace(subs[i]["old"], subs[i]["new"])
+main()
