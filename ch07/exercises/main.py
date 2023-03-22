5c6be20dcfeb52384aa0a8561250d60ea751b0e1
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-03-22 17:54:06 -0400

    erik

---
+++ ch07/exercises/main.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+print(type(t))
+print(type(1))
+
+#class are blueprint for objects
+# - functions are stored algorithms
+# - class as a stored data type
+# - classes are not executable
+# - don't put executabpe 
+
+class Point:
+    
