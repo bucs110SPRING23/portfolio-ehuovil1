be3e878f89f98c3851eab44063a2c4e5e33b040f
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-03-27 13:39:01 -0400

    erik

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -1,11 +1,14 @@
 class Rectangle:
     def __init__(self, x, y, h, w):
-        self.x = x
-        self.y = y
-        self.height = h
-        self.width = w
+        self.x = abs(x)
+        self.y = abs(y)
+        self.height = abs(h)
+        self.width = abs(w)
     def __str__(self):
-        print("x: " + self.x + "y: " + self.y + "height: " + self.height + "width: " + self.width)
+        #returns the properties of self
+        #args: none
+        #return: string of properties
+        return ("x: " + self.x + "y: " + self.y + "width: " + self.width + "height: " + self.height)
 
 
 
--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -1,5 +1,14 @@
-from Rectangle.py import Rectangle
+import Rectangle
 class Surface:
     def __init__(self, filename, x, y, h, w):
-        self.rect = x, y, h, w
+        self.rect = Rectangle(x, y, h, w)
         self.image = filename
+    def getRect(self):
+        # function description:  the rectangle data from self.rect, which is  the 
+        # function for a Rectangle using the x, y, h, and w, parameters
+        # args: none
+        # returns (self.rect): 
+        return self.rect
+
+
+
