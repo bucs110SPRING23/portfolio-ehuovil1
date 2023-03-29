e9da0a2fc1c55b5f2d8c1a024c93ebf9cf1e7c26
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-03-29 18:08:44 -0400

    erik

--- ch07/exercises/main.py
+++ ch07/exercises/main.py
@@ -1,5 +1,5 @@
 import turtle
-
+import pygame
 t = turtle.Turtle()
 print(type(t))
 print(type(1))
@@ -10,5 +10,30 @@ print(type(1))
 # - classes are not executable
 # - don't put executabpe 
 
-class Point:
+#class Point:
     
+# - snakecase: snake_case, underscores for spaces, all lowercase
+# - camelcase: camelCase, capital for spaces, starts lowercase
+
+points = []
+for p in range (10):
+    x = random.randint(0, 250)
+    y = random.randint(0, 250)
+    points.append(point.Point(x,y))
+
+
+
+
+
+
+t = turtle.Turtle()
+for p in points:
+    p.random_color()
+    t.color(p.color)
+    t.goto(p.xcoor, p.ycoor)
+
+turtle.Screen(). exitonclick()
+
+
+pygame.init()
+display = 
