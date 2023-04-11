aabd813140f48c99ac0e7b1cedc920ea69c01b88
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-04-10 23:19:19 -0400

    erik

--- ch08/lab/stringutility.py
+++ ch08/lab/stringutility.py
@@ -3,5 +3,25 @@ class StringUtility:
         self.string = string
     def __str__(self):
         return self.string
+    def vowels(self):
+        vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+        vowelcount = 0
+        for i in self.string:
+            for x in vowels:
+                if i == x:
+                    vowelcount +=1
+        if vowelcount < 5:
+            return str(vowelcount)
+        else:
+            return "many"
+    def bothEnds(self):
+            if len(self.string) < 3:
+                return ""
+            return (self.string[0] + self.string[1] + self.string[-1] + self.string[-2])
+           
+
+        
+
+
 
 
