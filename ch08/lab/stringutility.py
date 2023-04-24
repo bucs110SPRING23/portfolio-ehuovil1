0c4ea223871c5d75f91791921f98ff50ee8fac16
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-04-24 18:58:57 -0400

    erik

--- ch08/lab/stringutility.py
+++ ch08/lab/stringutility.py
@@ -19,33 +19,33 @@ class StringUtility:
                 return ""
             return (self.string[0] + self.string[1] + self.string[-2] + self.string[-1])
            
-    class StringManipulator:
-        def bothEnds(self, s):
-            if len(s) <= 2:
-                return ""
-            else:
-                return s[:2] + s[-2:]
+    #class StringManipulator:
+     #   def bothEnds(self, s):
+      #      if len(s) <= 2:
+       #         return ""
+        #    else:
+         #       return s[:2] + s[-2:]
 
-    def fixStart(self, s):
-        if len(s) <= 1:
-            return s
-        else:
-            first_char = s[0]
-            return first_char + s[1:].replace(first_char, '*')
+    #def fixStart(self, s):
+     #   if len(s) <= 1:
+      #      return s
+       # else:
+        #    first_char = s[0]
+         #   return first_char + s[1:].replace(first_char, '*')
 
-    def asciiSum(self, s):
-        return sum(ord(c) for c in s)
+    #def asciiSum(self, s):
+    #    return sum(ord(c) for c in s)
 
-    def cipher(self, s):
-        shifted = ""
-        for c in s:
-            if c.isalpha():
-                ascii_offset = 65 if c.isupper() else 97
-                shifted_char = chr((ord(c) - ascii_offset + len(s)) % 26 + ascii_offset)
-                shifted += shifted_char
-            else:
-                shifted += c
-        return shifted
+    #def cipher(self, s):
+     #   shifted = ""
+      #  for c in s:
+       #     if c.isalpha():
+        #        ascii_offset = 65 if c.isupper() else 97
+         #       shifted_char = chr((ord(c) - ascii_offset + len(s)) % 26 + ascii_offset)
+          #      shifted += shifted_char
+           # else:
+            #    shifted += c
+        #return shifted
 
         
 
