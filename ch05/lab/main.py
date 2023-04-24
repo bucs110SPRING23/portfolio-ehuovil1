d584e1f575ac2c426fd73d423c15ddd990b90b55
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-04-24 15:37:54 -0400

    erik

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 def threenp1(n):
     count = 0
     while n > 1.0:
@@ -13,28 +12,30 @@ def threenp1(n):
 
 def threenp1range(upperlimit):
     threenplus1_iters_dict = {}
-
-    for n in range(2,upperlimit + 1):
+    for n in range(2, upperlimit + 1):
         threenplus1_iters_dict[n] = threenp1(n) 
     return threenplus1_iters_dict
 
-threenp1(100)
-print(threenp1range(100).items())
-print(threenp1(100))
-
+# Create a Pygame window
+pygame.init()
 screen = pygame.display.set_mode((500, 500))
 pygame.display.update()
-pygame.draw.lines(screen, "white", False, list(threenp1range(100).items()))
+
+# Draw a line graph using the iterations from threenp1range()
+graph_data = threenp1range(100)
+max_iterations = max(graph_data.values())
+max_key = max(graph_data, key=graph_data.get)
+
+for key, value in graph_data.items():
+    pygame.draw.line(screen, "white", (key, 500), (key, 500 - value), 1)
+
+# Display the value with the maximum iterations on the graph
+font = pygame.font.Font(None, 30)
+text = font.render(f"Max Iterations: {max_iterations} (n = {max_key})", True, "white")
+screen.blit(text, (10, 10))
+
+# Update the display
 pygame.display.update()
-
-new_display = pygame.transform.flip(display, False, True)
-width, height = new_display.get_size()
-new_display = pygame.transform.scale(new_display, (width * <factor>, height * <factor>))
-display.blit(new_display, (0, 0))
-
-
-
-
 
 running = True
 while running:
@@ -43,12 +44,3 @@ while running:
             running = False
 
 pygame.quit()
-
-
-
-
-
-
-
-
-
