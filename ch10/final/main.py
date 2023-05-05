1e6f9dc62d8290a52924f53d2f596a831082e2b1
ehuovil1 <you@example.comehuovil1@binghamton.edu>
2023-05-05 12:53:36 -0400

    erik

---
+++ ch10/final/main.py
@@ -0,0 +1,19 @@
+import pygame
+from controller import Controller
+from controller import IpBase
+# import your controller
+
+
+def main():
+    pygame.init()
+    control = Controller()
+    control.main()
+    # Create an instance on your controller object
+    # Call your mainloop
+
+    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
+
+
+# https://codefather.tech/blog/if-name-main-python/
+if __name__ == "__main__":
+    main()
